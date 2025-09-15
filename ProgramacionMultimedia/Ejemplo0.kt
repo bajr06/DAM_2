713a868aee6a1b89382b497481dc6637570a2a74
bajr06 <181849388+bajr06@users.noreply.github.com>
2025-09-15 14:29:41 +0200

    Ejemplos - Programacion Multimedia

---
+++ ProgramacionMultimedia/Ejemplo0.kt
@@ -0,0 +1,47 @@
+package com.example.aplicacion_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplicacion_1.ui.theme.Aplicacion_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aplicacion_1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable // Funciones que usan el Jet Pack.
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier // Parámetro para situarlo en la pantalla del dispositivo.
+    )
+}
+
+@Preview(showBackground = true) // Poder ser vista en el dispositivo.
+@Composable
+fun GreetingPreview() {
+    Aplicacion_1Theme {
+        Greeting("Android")
+    }
+}
