f1ca7a2732fd5264ea1df53878c9a66b885503a8
bajr06 <181849388+bajr06@users.noreply.github.com.>
2025-09-16 23:29:37 +0200

    Avance - Base de Datos

--- AccesoADatos/Ejercicio2.java
+++ AccesoADatos/Ejercicio2.java
@@ -2,6 +2,12 @@
 
 public class Ejercicio2 {
 	public static void main(String [] args) {
+		File fichero = new File();
 
+		if(fichero.exists()) {
+			fichero.delete();
+		} else {
+			System.out.println("No existe el archivo.");
+		}
 	}
 }
---
+++ AccesoADatos/Ejercicio3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+	static Scanner entrada = new Scanner(System.in);
+
+	public class void main(String [] args) {
+		System.out.println("¡Bienvenidos al creador de Directorios!");
+		System.out.println("Introduce el nombre del directorio en donde quieres crear el fichero:");
+		String nombreDirectorio = entrada.nextLine();
+
+		System.out.println("Ahora introduzca el nombre del fichero que quieres que cree:");
+		String nuevoFichero = entrada.nextLine();
+
+
+	}
+}
