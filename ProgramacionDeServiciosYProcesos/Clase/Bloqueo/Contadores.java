d052c634dcf8c09478712d454031cf6fe47dbfe8
bajr06 <181849388+bajr06@users.noreply.github.com>
2025-10-09 14:19:17 +0200

    Contadores - Programacion de Servicios y Procesos

---
+++ ProgramacionDeServiciosYProcesos/Clase/Bloqueo/Contadores.java
@@ -0,0 +1,33 @@
+package Bloqueo;
+
+public class Contadores {
+	private long cont1 = 0;
+	private long cont2 = 0;
+	private final Object lock1 = new Object();
+	private final Object lock2 = new Objetc();
+
+
+	public void incrementar1() {
+		synchronized(lock1) {
+			cont1++;
+		}
+	}
+
+	public void incrementar2() {
+		synchronized(lock2) {
+			cont2++;
+		}
+	}
+
+	public long getContador1() {
+		synchronized(lock1) {
+			return cont1;
+		}
+	}
+
+	public long getContador2() {
+		synchronized(lock2) {
+			return cont2;
+		}
+	}
+}
