f541e5e1d57141d37dc7e1549e62846e870b22bc
bajr06 <181849388+bajr06@users.noreply.github.com>
2025-10-14 13:36:59 +0200

    Interbloqueo avance - Programacion de Servicios y Procesos

---
+++ ProgramacionDeServiciosYProcesos/Clase/Interbloqueo/Principal.java
@@ -0,0 +1,30 @@
+package InterBloqueo;
+
+import java.io.*;
+
+public class Principal {
+	public static void main(String [] args) {
+		CuentaBancaria c1 = new CuentaBancaria("ES21 5465 4546 464514", 12500);
+		CuentaBancaria c2 = new CuentaBancaria("ES74 4514 6456 165756", 23000);
+
+		System.out.printf("Saldo inicial de %s: %d\n", c1.getNumCuenta(), c1.getSaldo());
+		System.out.printf("Saldo inicial de %s: %d\n", c2.getNumCuenta(), c2.getSaldo());
+		System.out.println("-----------------------");
+
+		Thread h1 = new Thread(new Hilo(c1, c2, "H1"));
+		Thread h2 = new Thread(new Hilo(c1, c2, "H2"));
+
+		h1.start();
+		h2.start();
+
+		try {
+			h1.join();
+			h2.join();
+		} catch(InterrupedException ie) {
+			ie.printStackTrace();
+		}
+
+		System.out.printf("Saldo final de %s: %d\n", c1.getNumCuenta(), c1.getSaldo());
+		System.out.printf("Saldo final de %s: %d\n", c2.getNumCuenta(), c2.getSaldo());
+	}
+}
