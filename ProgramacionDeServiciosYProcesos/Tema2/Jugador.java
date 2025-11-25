776d3f45a4a43f192e515e6cef42d00f22006daf
bajr06 <181849388+bajr06@users.noreply.github.com>
2025-11-25 13:22:14 +0100

    Arreglos 8 - Programacion de Servicios y Procesos

--- ProgramacionDeServiciosYProcesos/Tema2/Jugador.java
+++ ProgramacionDeServiciosYProcesos/Tema2/Jugador.java
@@ -1,11 +1,10 @@
-package Tema2;
+/*package Tema2;
 
 import java.util.concurrent.Semaphore;
 
 private final static Semaphore SEM1 = new Semaphore(1);
 private final static Semaphore SEM2 = new Semaphore(0);
 
-SEM1.release(); /* +1 */
-SEM1.acquire(); /* -1 */
-
+SEM1.release();  +1
+SEM1.acquire();  -1 */
 
